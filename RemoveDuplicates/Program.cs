b4d9b72a679b8f746cdab8d2d15bb794c134b23c
Hardobas1356 <kalinhabacha@gmail.com>
2025-05-09 22:13:29 +0300

    New exercise

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace RemoveDuplicates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.RemoveDuplicates([1, 2]));
+    }
+
+    public static class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+
+            int leftPointer = 0;
+            int rightPointer = 0;
+            int count = 1;
+
+            while (rightPointer < nums.Length - 1 && leftPointer < nums.Length)
+            {
+                while (rightPointer < nums.Length - 1 && nums[leftPointer] == nums[rightPointer])
+                {
+                    rightPointer++;
+                }
+
+                nums[++leftPointer] = nums[rightPointer];
+
+                if (nums[leftPointer - 1] != nums[leftPointer])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
