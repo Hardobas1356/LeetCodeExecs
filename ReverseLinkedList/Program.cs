5d7cfb95e43d5b14c5631bc57055285e89258a2c
Hardobas1356 <kalinhabacha@gmail.com>
2025-05-18 13:05:58 +0300

    new exercises

--- ReverseLinkedList/Program.cs
+++ ReverseLinkedList/Program.cs
@@ -5,8 +5,9 @@ internal class Program
     static void Main(string[] args)
     {
         ListNode listNode = new ListNode();
-        ListNode nodes = CreateNodes([1, 2, 3, 4, 5]);
-        Solution.ReverseList(nodes);
+        ListNode nodes = CreateNodes([1,2]);
+
+        Solution.RemoveNthFromEnd(nodes, 2);
     }
 
     public static ListNode CreateNodes(int[] array)
@@ -57,6 +58,149 @@ internal class Program
             }
 
             return newList;
+        }
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list2 == null) return list1;
+            if (list1 == null) return list2;
+
+            ListNode newList;
+            if (list1.val < list2.val)
+            {
+                newList = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            else
+            {
+                newList = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+
+            ListNode current = newList;
+
+            while (list1 != null || list2 != null)
+            {
+
+                if (list1 != null && list2 != null)
+                {
+                    if (list1.val < list2.val)
+                    {
+                        current.next = new ListNode(list1.val);
+                        list1 = list1.next;
+                    }
+                    else
+                    {
+                        current.next = new ListNode(list2.val);
+                        list2 = list2.next;
+                    }
+
+                    current = current.next;
+                }
+                else
+                {
+                    while (list1 != null)
+                    {
+                        current.next = new ListNode(list1.val);
+                        list1 = list1.next;
+
+                        current = current.next;
+                    }
+
+                    while (list2 != null)
+                    {
+                        current.next = new ListNode(list2.val);
+                        list2 = list2.next;
+
+                        current = current.next;
+                    }
+                }
+            }
+
+            return newList;
+        }
+
+        public static bool HasCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static ListNode MiddleNode(ListNode head)
+        {
+            if (head.next == null) return head;
+
+            int count = 0;
+
+            ListNode listNode = head;
+
+            while (head.next != null)
+            {
+                count++;
+                head = head.next;
+            }
+
+            count = count / 2 + count % 2;
+
+            while (count > 0)
+            {
+                listNode = listNode.next;
+                count--;
+            }
+
+            return listNode;
+        }
+
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            int slowCounter = 1;
+            int fastCounter = 1;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                fast = fast.next.next;
+                fastCounter += 2;
+            }
+
+            if (fast.next != null)
+            {
+                fast = fast.next;
+                fastCounter++;
+            }
+
+            if (n == fastCounter)
+            {
+                return head.next;
+            }
+
+            int target = fastCounter - n;
+            while (slowCounter < target)
+            {
+                slow = slow.next;
+                slowCounter++;
+            }
+
+            ListNode swap;
+            if (fast == slow)
+                swap = null;
+
+            else swap = slow.next.next;
+            slow.next = swap;
+
+            return head;
         }
     }
 }
---
+++ SortColors/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SortColors;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution.SortColors([0, 1, 1, 2, 0, 1, 2]);
+    }
+
+    public static class Solution
+    {
+        public static void SortColors(int[] nums)
+        {
+
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (map.ContainsKey(num))
+                    map[num]++;
+                else map[num] = 1;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (map.ContainsKey(0) && map[0] > 0)
+                {
+                    nums[i++] = 0;
+                    map[0]--;
+                }
+                while (map.ContainsKey(1) && map[1] > 0)
+                {
+                    nums[i++] = 1;
+                    map[1]--;
+                }
+                while (map.ContainsKey(2) && map[2] > 0)
+                {
+                    nums[i++] = 2;
+                    map[2]--;
+                }
+            }
+        }
+    }
+}
